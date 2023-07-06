a0dd674a82e0d70ef7e20f25ec5d8e233b02bf73
Jackie <jackie15910@gmail.com>
2023-07-06 19:38:31 -0400

    Added breedfetcher, index and text, eslinted

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,19 @@
 const request = require('request');
-let url = 'https://api.thecatapi.com/v1/breeds/search?q=';
-let catName = process.argv.slice(2);
 
-request(url + catName, (error, response, data) => {
-  if (error) {
-    console.log('error:', error);
-    return;
+const fetchBreedDescription = function(breedName, callback) {
+  let url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName
+  request(url, (error, response, data) => {
+    if (error) {
+      return callback(error, null);
+    }
+    const parsedData = JSON.parse(data);
+    const breedData = parsedData[0];
+    if (breedData) {
+      return callback(null, breedData.description);
+    }
+    else {
+      return callback("Breed not found.", null);
   }
-  const parsedData = JSON.parse(data);
-  if (parsedData[0]) {
-    //console.log(typeof data);
-    console.log(parsedData[0]);
-    //console.log(typeof parsedData);
-    //console.log(Object.keys(parsedData[0]));
-  } else {
-    console.log("Cat name not found");
-  }
-});
+  })
+};
+module.exports = { fetchBreedDescription };
